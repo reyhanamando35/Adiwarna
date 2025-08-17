37bd91b9512dc033f8b79d6eb6f1d482b8a0ab74
Joseph-beginner-programmer <joseptan1512@outlook.com>
2025-08-17 19:58:30 +0700

    rensponsive

--- resources/views/curatedWorks/partials/title.blade.php
+++ resources/views/curatedWorks/partials/title.blade.php
@@ -1,7 +1,17 @@
-<div id="curated-title">
-    <h1 class="font-['Sen',sans-serif] tracking-[0.2em] text-[#67753E] text-6xl">Introducing Our</h1>
+<div id="curated-title" class="h-[100dvh] flex flex-col sm:justify-start md:justify-center">
+    <h1 class="font-['Sen',sans-serif] tracking-[0.2em] text-[#67753E] 
+        
+         md:text-[clamp(3rem,4vw,6rem)]
+         lg:text-[clamp(3rem,5vw,8rem)] 
+         xl:text-[clamp(4rem,6vw,10rem)]">Introducing Our</h1>
     <div class="space-y-2">
-        <h1 class="m-0 font-['Maragsa'] text-[#A14000] font-bold text-[200px] leading-none">Curated</h1>
-        <h1 class="m-0 font-['Maragsa'] text-[#A14000] font-bold text-[200px] leading-none">Works</h1>
+        <h1 class="m-0 font-['Maragsa'] text-[#A14000] font-bold text-[clamp(2rem,8vw,4rem)] 
+         md:text-[clamp(3rem,10vw,8rem)]
+         lg:text-[clamp(3rem,11vw,10rem)]
+         xl:text-[clamp(4rem,12vw,12rem)] leading-none">Curated</h1>
+        <h1 class="m-0 font-['Maragsa'] text-[#A14000] font-bold text-[clamp(2rem,8vw,4rem)]    
+         md:text-[clamp(3rem,10vw,8rem)]
+         lg:text-[clamp(3rem,11vw,10rem)]
+         xl:text-[clamp(4rem,12vw,12rem)] leading-none">Works</h1>
     </div>
-</div>
+</div> 
