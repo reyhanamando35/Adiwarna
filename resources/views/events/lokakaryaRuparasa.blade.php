98c883bfe010abb03cbcb6f7d2cc70b08b815fe0
c14230088 <Tanaka272cool@gmail.com>
2025-08-22 23:09:20 +0700

    FEAT: reducing image opacity on event pages

--- resources/views/events/lokakaryaRuparasa.blade.php
+++ resources/views/events/lokakaryaRuparasa.blade.php
@@ -6,14 +6,14 @@
     <div
         class="w-screen max-w-screen min-h-screen overflow-hidden relative flex flex-col bg-[var(--beige)] justify-center items-center z-[988]">
 
-        <div class="absolute z-[1] left-[-40%] top-[25%] h-[90%] w-full bg-[linear-gradient(180deg,_#7593ad_0%,_rgba(117,147,173,0)_100%)]"
+        <div class="absolute z-[1] opacity-[50%] left-[-40%] top-[25%] h-[90%] w-full bg-[linear-gradient(180deg,_#7593ad_0%,_rgba(117,147,173,0)_100%)]"
             style="-webkit-mask: url('{{ asset('assets/fingerprint/charista2.png') }}') no-repeat center / contain;
             mask: url('{{ asset('assets/fingerprint/charista2.png') }}') no-repeat center / contain;
             filter: drop-shadow(3px 3px 3px white);
             transform: rotate(-8deg);">
         </div>
 
-        <div class="absolute z-[1] right-[-5.75%] bottom-[-42%] h-full w-full bg-[linear-gradient(180deg,_rgba(117,147,173,40)_0%,_rgba(117,147,173,0)_100%)]"
+        <div class="absolute z-[1] opacity-[60%] right-[-5.75%] bottom-[-42%] h-full w-full bg-[linear-gradient(180deg,_rgba(117,147,173,40)_0%,_rgba(117,147,173,0)_100%)]"
             style="-webkit-mask: url('{{ asset('assets/fingerprint/nayla_fingerprint1.png') }}') no-repeat center right / contain;
             mask: url('{{ asset('assets/fingerprint/nayla_fingerprint1.png') }}') no-repeat center right / contain;
             filter: drop-shadow(3px 3px 3px white);">
