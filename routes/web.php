87ebdb1e87df7c4cd8667fc8628c10c64c2f2d28
reyhanamando35 <reyhan.amando@gmail.com>
2025-08-24 23:05:53 +0700

    merch done

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('merch');
 });
+
+Route::get('/merch', function () {
+    return view('merch');
+});
+
